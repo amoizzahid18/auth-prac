0447fe160df04fb1a6650c37d90c34d6e55e6609
amoizzahid18 <a.moiz.zahid.18@gmail.com>
2025-02-05 22:12:25 +0500

    added middleware

---
+++ backend/middlewares/authValidation.js
@@ -0,0 +1,27 @@
+const JOI = require("joi");
+
+const signupValidation = (req, res, next) => {
+  const schema = JOI.object({
+    name: JOI.string().min(3).max(50).required(),
+    email: JOI.string().email().required(),
+    password: JOI.string().min(4).max(30).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) return req.status(400).json({ message: "Bad request", error });
+  next();
+};
+
+const loginValidation = (req, res, next) => {
+  const schema = JOI.object({
+    email: JOI.string().email().required(),
+    password: JOI.string().min(4).max(30).required(),
+  });
+  const { error } = schema.validate(req.body);
+  if (error) return req.status(400).json({ message: "Bad request", error });
+  next();
+};
+
+module.exports = {
+  signupValidation,
+  loginValidation,
+};
